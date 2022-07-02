a934c3de85921290c40a7b60cf941104576ce80a
Ujjwal Jayswal <ujjwaljayswal9@gmail.com>
2022-07-02 09:09:08 +0530

    bug fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
-import Newsletter from '../components/Newsletter'
+import Newsletter from '../components/newsletter'
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
