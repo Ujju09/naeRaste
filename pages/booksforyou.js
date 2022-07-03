1d03d2c971f676dd7cbd5d0d56c7177203c0df98
Ujjwal Jayswal <ujjwaljayswal9@gmail.com>
2022-07-03 09:22:31 +0530

    added orbit

--- pages/booksforyou.js
+++ pages/booksforyou.js
@@ -2,48 +2,74 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
+import Script from "next/script";
 
 export default function BooksForYou({ records }) {
   return (
     <div className={styles.container}>
+      <Script src="https://js.withorbit.com/orbit-web-component.js" />
       <Head>
         <title>🙀 Books For You</title>
         <meta name="description" content="Handpicked from the library" />
       </Head>
       <main className={styles.main}>
         <h1 className={styles.title}>Books For You</h1>
-        <p>
-          These books are curated to bring the best out of you. And obviously
-          they don&apos;t teach this at school.
-        </p>
-        <div className={styles.grid}>
+        <p>Obviously they don&apos;t teach you this at school.</p>
+        <div className={styles.gridcal}>
           {records.map((record, index) => (
-            <div key={index} className={styles.card}>
-              <a
-                href={record.fields["buylink"]}
+            <a
+              href={record.fields["buylink"]}
+              style={{
+                textDecoration: "none",
+              }}
+              key={index}
+            >
+              <Image
+                src={record.fields["Url"]}
+                alt={record.fields["Title"]}
+                width={200}
+                height={300}
+              />
+              <h3
                 style={{
-                  textDecoration: "none",
+                  textOverflow: "ellipsis",
                 }}
               >
-                <Image
-                  src={record.fields["Url"]}
-                  alt={record.fields["Title"]}
-                  width={200}
-                  height={300}
-                />
-                <h3>{record.fields["Title"]}</h3>
-                <p
-                  style={{
-                    fontSize: "1rem",
-                    color: "grey",
-                  }}
-                >
-                  {record.fields["Author"]}
-                </p>
-              </a>
-            </div>
+                {record.fields["Title"]}
+              </h3>
+              <p
+                style={{
+                  fontSize: "0.8rem",
+                  color: "grey",
+                  textOverflow: "ellipsis",
+                }}
+              >
+                {record.fields["Author"]}
+              </p>
+            </a>
           ))}
         </div>
+        <orbit-reviewarea
+          color="blue"
+          style={{
+            width: "100%",
+            padding: "1rem",
+          }}
+        >
+          <orbit-prompt
+            question="What's the working name for Andy's experimental mnemonic medium platform?"
+            answer="Orbit"
+          ></orbit-prompt>
+          <orbit-prompt
+            question="What kind of quantum gate is this?"
+            question-attachments="https://docs.withorbit.com/toffoli.png"
+            answer="A Toffoli gate."
+          ></orbit-prompt>
+          <orbit-prompt
+            question="Given a right triangle with legs of length $a$ and $b$, what is the length of hypotenuse $c$?"
+            answer="$$c = \sqrt{a^2 + b^2}$$"
+          ></orbit-prompt>
+        </orbit-reviewarea>
       </main>
     </div>
   );
