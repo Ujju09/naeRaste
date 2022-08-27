b5648dd12026f2797a3c10047d7cc8c7287fd1f0
Ujjwal Jayswal <ujjwaljayswal9@gmail.com>
2022-08-27 19:18:55 +0530

    Links to external sites, CTA and others

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,8 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Link from 'next/link'
-// import Newsletter from '../components/newsletter'
-
 import Script from 'next/script'
+import Link from 'next/link'
 const Home: NextPage = () => {
 
   
@@ -27,7 +25,7 @@ const Home: NextPage = () => {
         <div style={{
           padding: '1rem',
         }}>
-          <Image src="/banner.png" alt="notebook" width={428} height={234} />
+          <Image src="/banner.png" alt="notebook" width={780} height={299} />
         </div>
         <div style={{
           padding: '1rem',
@@ -46,29 +44,71 @@ const Home: NextPage = () => {
             gap: '1rem',
           }}
           >
-            <Image src="/NLM1.png" alt="notebook" width={150} height={200} />
-            <Image src="/NLM2.png" alt="notebook" width={150} height={200} />
+            <Image src="/NLM1.png" alt="notebook" width={200} height={250} />
+            <Image src="/NLM2.png" alt="notebook" width={200} height={250} />
            
-            <Image src="/Golden ratio.png" alt="notebook" width={150} height={200} />
+            <Image src="/Golden ratio.png" alt="notebook" width={200} height={250} />
           </div>
-          <p>
-            Exercise notebooks.
+          <p style={{
+            textAlign: "center",
+          }}>
+            Exercise notebooks <br/>
+             Affordable, Accessible, Aspirational
           </p>
-          <Image src="/arrow-6.svg" alt="notebook" width={428} height={234} />
+          <Image src="/arrow-6.svg" alt="notebook" width={150} height={150} />
           Simple product - High impact 
+          <p style={{
+            textAlign: 'center',
+            padding: '1.5rem',
+          }}>
+            Every notebook is embedded with QR code that will unlock the best learning resources from around the world.<br />
+            Which means it doesn&apos;t matter if you live in Delhi or a small town in Bihar. Everyone will be able to access these resources free of cost.
+          </p>
+          <Link href={`https://begin.naeraste.com/`}>
+          <div className={styles.button} >
+            Let&apos;s begin
+          </div>
+          </Link>
+         
         </div>
-        <div >
-          
-          
-          
+        <div >  
         </div>
+        
+
       </main>
 
-      {/* <Newsletter /> */}
+
      
       
      
       <Script async data-uid="37bab4a468" src="https://artisanal-producer-6695.ck.page/37bab4a468/index.js"></Script>
+      <footer className={styles.footer}>
+        <a href={`https://research.naeraste.com/`} style={{
+          textDecoration: "none",
+          color:"black"
+        }}>
+        <div className={styles.card}>
+            Want to participate in research aimed to help you learn better?
+            <button className={styles.secondaryButton}>
+            →
+            </button>
+          </div>
+        </a>
+        
+        
+        <a href={`https://www.youtube.com/channel/UCGypumUZB0u4Q7Rno87DpZw`} style={{
+          textDecoration: "none",
+          color:"black"
+        }}>
+        <div className={styles.card}>
+            Go to our YouTube channel. We publish videos on learning and problem solving.
+            <button className={styles.secondaryButton}>
+            →
+            </button>
+          </div>
+        </a>
+          
+      </footer>
      
     </div>
   )
@@ -77,12 +117,3 @@ const Home: NextPage = () => {
 export default Home
 
 
-{/* <div style={{
-          backgroundColor: 'purple',
-          width: '100%',
-          display: 'flex',
-          justifyContent: 'center',
-        }}>
-      
-          <Image src="/Frame 4.png" alt="notebook" width={144} height={100} />
-        </div> */}
