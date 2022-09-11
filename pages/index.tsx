193bc612dc90564b58117c410ee8bcae76820a42
Ujjwal Jayswal <ujjwaljayswal9@gmail.com>
2022-09-11 15:40:50 +0530

    subtle

--- pages/index.tsx
+++ pages/index.tsx
@@ -103,13 +103,14 @@ const Home: NextPage = () => {
 
             </li>
             <li>
+              © nae raste {new Date().getFullYear()}
+            </li>
+            <li>
             <a href="https://m.youtube.com/channel/UCGypumUZB0u4Q7Rno87DpZw/featured">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="13" className="handle"><path d="M15.6 2c.4 1.4.4 4.4.4 4.4s0 3-.4 4.4c-.2.8-.8 1.4-1.5 1.6-1.4.4-6.1.4-6.1.4s-4.7 0-6-.4c-.8-.2-1.4-.8-1.6-1.6C0 9.4 0 6.4 0 6.4S0 3.4.4 2C.6 1.2 1.2.6 1.9.4 3.3 0 8 0 8 0s4.7 0 6 .4c.8.2 1.4.8 1.6 1.6zM6.4 9.2l4.8-2.8-4.8-2.8v5.6z"></path></svg>
               </a>
             </li>
-            <li>
-              nae raste
-            </li>
+            
           </ul>
         </div>
         <Script async data-uid="cad6bef98a" src="https://artisanal-producer-6695.ck.page/cad6bef98a/index.js"></Script>
